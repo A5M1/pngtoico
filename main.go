664a98e92f1b88b4c5c5eb0b18f17ebd18e2b81e
Beanos <admin@abby0666.xyz>
2024-03-22 11:54:49 -0700

    i done fucked up
    shit

--- main.go
+++ main.go
@@ -14,15 +14,18 @@ func main() {
 		fmt.Println("Usage: drag_png_to_exe <input_png_file>")
 		os.Exit(1)
 	}
+	
 	inputFile := os.Args[1]
 	if !strings.HasSuffix(strings.ToLower(inputFile), ".png") {
 		fmt.Println("Error: Input file must be a PNG.")
 		os.Exit(1)
 	}
+	
 	if _, err := os.Stat(inputFile); os.IsNotExist(err) {
 		fmt.Printf("Error: File '%s' not found.\n", inputFile)
 		os.Exit(1)
 	}
+	
 	outputFile := strings.TrimSuffix(inputFile, filepath.Ext(inputFile)) + ".ico"
 	batFilePath := filepath.Join(os.TempDir(), "conversion.bat")
 	err := createBatchFile(batFilePath, inputFile, outputFile)
@@ -30,6 +33,7 @@ func main() {
 		fmt.Printf("Error creating batch file: %v\n", err)
 		os.Exit(1)
 	}
+	
 	cmd := exec.Command("cmd", "/C", batFilePath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
